b66f01f361b6d162c8078de1e27bb499c5852eea
Nel <nel.renaudin@gmail.com>
2024-01-08 09:04:12 +0100

    create user model

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, length: { min: 8 } },
+})
+
+module.exports = mongoose.model("User", userSchema)
