6a7d271ea9b8ff0bebbe71f78bfafa5446de86ff
Nel <nel.renaudin@gmail.com>
2024-01-08 09:02:39 +0100

    connect to db

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
+require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// connect to db
+const MONGO_URL = process.env.MONGO_URL
+const main = async () => mongoose.connect(MONGO_URL)
+main().catch((err) => console.error(err))
 
 app.get("/", (req, res) => res.send("Hello World!"))
 
